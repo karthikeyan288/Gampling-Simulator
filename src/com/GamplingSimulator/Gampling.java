adf78d7740267823a7e9a187467abc8c4c0e2985
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-19 13:58:11 +0530

    ADDED : checking possibility of win or loss

---
+++ src/com/GamplingSimulator/Gampling.java
@@ -0,0 +1,16 @@
+package com.GamplingSimulator;
+
+public class Gampling {
+	private static final int totalStake = 100;
+	private static final int perDayBet =1; 
+	private static  int playerRandom;
+    static int remainingStake ;
+    
+    public static void game() {
+    	System.out.println("The game starts with total shake of $"+totalStake);
+    }
+    public static void main(String[] args) {
+    	game();
+	
+}
+}
